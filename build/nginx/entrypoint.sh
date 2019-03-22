31307e80df1214c31c26bc23eab85f45649e931a
Kevin Meredith <kevin@meredithkm.info>
2019-03-22 16:52:57 +0000

    Add SSL config to nginx container

--- build/nginx/entrypoint.sh
+++ build/nginx/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 
+# Generate dhparam.pem if it doesn't already exist
+if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
+    openssl dhparam -dsaparam -out /etc/letsencrypt/ssl-dhparams.pem 4096
+fi
+
+# Copy ssl options if it doesn't exist
+if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
+    cp /ssl-config/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf
+fi
+
+chown -R www-data:www-data /etc/letsencrypt
+
 if [[ -z ${1} ]]; then
-  chown www-data:www-data /etc/nginx
+  chown -R www-data:www-data /etc/nginx
   if [[ $ENABLE_WORDPRESS = 1 ]]; then 
     cat /etc/nginx/conf.d/wp.tmpl | sed "s|WORDPRESS_URL|$WORDPRESS_URL|g" > /etc/nginx/conf.d/wp.conf
   fi
